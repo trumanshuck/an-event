6995ebee1af5aa994bc492f7faedf103a95b5808
Truman Shuck <hello@trumanshuck.com>
2020-08-19 13:55:53 -0500

    Update lng-lat to x-y translation, add journey lines

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,18 +1,20 @@
 module HomeHelper
+  # https://stackoverflow.com/a/14457180
+  # plus some magic values
   def laty(lat)
-    northernmost = 49.384472
-    southernmost = 24.446667
-    map_offset = 8.5
-    diff = northernmost - southernmost
+    width = 320
+    height = 160
+    lat_rad = lat * Math::PI / 180
+    tan = Math.tan((Math::PI / 4) + (lat_rad / 2))
+    mercator_n = Math.log(tan, Math::E)
 
-    ((northernmost - lat) / diff * 100) + map_offset
+    (height/2)-(width*mercator_n/(2*Math::PI))
   end
 
+  # https://stackoverflow.com/a/14457180
+  # plus some magic values
   def lngx(lng)
-    easternmost = -66.949778.abs
-    westernmost = -124.733056.abs
-    diff = westernmost - easternmost
-
-    (westernmost - lng.abs) / diff * 100
+    width = 540.0
+    ((lng + 180.0) * (width / 360.0)) - 74
   end
 end
