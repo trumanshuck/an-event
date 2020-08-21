d0f280b270b35f1f3bb6fef546f553bc2bc45566
Truman Shuck <hello@trumanshuck.com>
2020-08-20 20:31:17 -0500

    Add presence validations for post, stop

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :stop
   has_rich_text :content
 
+  validates :title, :slug, presence: true
+
   delegate :to_param, to: :slug
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,5 +2,7 @@ class Stop < ApplicationRecord
   has_one_attached :splash
   has_many :posts
 
+  validates :name, :from, :to, :lat, :lng, :slug, presence: true
+
   delegate :to_param, to: :slug
 end
