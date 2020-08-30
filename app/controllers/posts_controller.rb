fc7cea92b99139bc43a0f3f53b498dae4608025d
Truman Shuck <hello@trumanshuck.com>
2020-08-30 10:26:19 -0500

    Add thoughts scaffold

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
-  end
-
   # GET /posts/1
   # GET /posts/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :thoughts
   resources :posts
   resources :stops
   resources :registry, only: [:index]
