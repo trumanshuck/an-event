6fe3e09d282fe799aac3918f5aabcedf7c209c0f
Truman Shuck <hello@trumanshuck.com>
2020-08-30 00:01:02 -0500

    Allow assets to fall back in compilation, remove postcss thing to strip classes

--- postcss.config.js
+++ postcss.config.js
@@ -13,19 +13,5 @@ const environment = {
 };
 
 // Add everything below!
-if (process.env.RAILS_ENV === 'production') {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/.html.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-        './app/javascript/**/*.vue',
-        './app/javascript/**/*.jsx',
-      ],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
 
 module.exports = environment;
