36618745f90ef1397fec16eacc9f0151fe73cb6e
Truman Shuck <hello@trumanshuck.com>
2020-09-12 10:41:49 -0500

    Make splash images, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'sessions/create'
-  resources :thoughts
+  resources :thoughts, path: "send-your-love"
   resources :posts
   resources :stops, path: "journal"
   resources :registry, only: [:index]
